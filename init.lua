7a598aa6cb9f4577f8708c94aedccee992b0a6e4
Andrey Bondarenkov <me@bondarenkov.ru>
2024-04-17 14:51:26 +0500

    added: lazy.nvim & colorscheme

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Vim Config
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -21,3 +22,23 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
+
+-- Plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+ "rebelot/kanagawa.nvim" 
+})
+
+vim.cmd.colorscheme("kanagawa-wave")
