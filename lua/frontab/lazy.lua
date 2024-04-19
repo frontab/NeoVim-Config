43d81d3ca40af6ae0d1205081937c5f411528d7f
Andrey Bondarenkov <me@bondarenkov.ru>
2024-04-19 16:10:53 +0500

    added lsp & mason

--- lua/frontab/lazy.lua
+++ lua/frontab/lazy.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("frontab.plugins", {
+require("lazy").setup({ { import = "frontab.plugins" }, { import = "frontab.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
